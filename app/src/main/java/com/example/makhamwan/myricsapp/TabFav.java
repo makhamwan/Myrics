3ce18e2d0b664a74ebe13189eda7f59a486451e0
Makhamwan <mkhv.bt96@gmail.com>
2017-05-17 17:30:07 +0700

    Modify fragment class and mainactivity

---
+++ app/src/main/java/com/example/makhamwan/myricsapp/TabFav.java
@@ -0,0 +1,19 @@
+package com.example.makhamwan.myricsapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by makhamwan on 5/17/2017 AD.
+ */
+
+public class TabFav extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fav_tab, container, false);
+    }
+}
